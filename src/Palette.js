56ecaa1a588814445df2b5f7ecd1f7313b9e6df0
Nawanit <sharma.nawanit123@gmail.com>
2020-09-18 15:46:24 +0530

    Created Palette Component

---
+++ src/Palette.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Palette extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="Palette">
+        {/* Navbar Goes Here*/}
+        <div className="Palette-colors">{/* Bunch of Colors*/}</div>
+        {/* Footer Goes Here */}
+      </div>
+    );
+  }
+}
+
+export default Palette;
