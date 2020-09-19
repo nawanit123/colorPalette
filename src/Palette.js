b7477d16387e6f24f37368caaa60ddce3acb2553
Nawanit <sharma.nawanit123@gmail.com>
2020-09-19 22:16:37 +0530

    Added rc slider to the project

--- src/Palette.js
+++ src/Palette.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
 import './Palette.css';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
 class Palette extends Component {
-  state = {};
+  state = {
+    level: 500,
+  };
+  changeLevel = (level) => {
+    this.setState({ level });
+  };
   render() {
-    const colorBoxes = this.props.colors.map((color) => (
-      <ColorBox backgroundColor={color.color} name={color.name} />
+    const { colors } = this.props.palette;
+    const { level } = this.state;
+    const colorBoxes = colors[level].map((color) => (
+      <ColorBox backgroundColor={color.hex} name={color.name} />
     ));
     return (
       <div className="Palette">
         {/* Navbar Goes Here*/}
+        <Slider
+          defaultValue={level}
+          min={100}
+          max={900}
+          step={100}
+          onAfterChange={this.changeLevel}
+        />
         <div className="Palette-colors">{colorBoxes}</div>
         {/* Footer Goes Here */}
       </div>
