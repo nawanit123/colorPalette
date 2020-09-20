54ccb110ae0e05dcab9e2728761aeb877c025b33
Nawanit <sharma.nawanit123@gmail.com>
2020-09-20 20:41:59 +0530

    Added Snackbar Component

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,15 +3,22 @@ import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 import './Navbar.css';
 
 class Navbar extends Component {
   state = {
     format: 'hex',
+    open: false,
   };
   handleChange = (evt) => {
-    this.setState({ format: evt.target.value });
+    this.setState({ format: evt.target.value, open: true });
     this.props.handleChange(evt.target.value);
+  };
+  closeSnackbar = () => {
+    this.setState({ open: false });
   };
   render() {
     const { level, changeLevel } = this.props;
@@ -40,6 +47,28 @@ class Navbar extends Component {
             <MenuItem value="rgba">RGB -rgba(255,255,255,1.0)</MenuItem>
           </Select>
         </div>
+        <Snackbar
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+          open={this.state.open}
+          autoHideDuration={3000}
+          message={
+            <span id="message-id">
+              Format Changed to {format.toUpperCase()}
+            </span>
+          }
+          ContentProps={[('aria-describedby': 'message-id')]}
+          onClose={this.closeSnackbar}
+          action={[
+            <IconButton
+              onClick={this.closeSnackbar}
+              color="inherit"
+              key="close"
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>,
+          ]}
+        />
       </header>
     );
   }
