948d45112f1559644ed4e02a408de58f57e2d737
Nawanit <sharma.nawanit123@gmail.com>
2020-09-21 15:23:51 +0530

    Connected React Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Palette from './Palette';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import seedColors from './seedColor.js';
 import { generatePalette } from './colorHelpers';
@@ -8,9 +9,18 @@ class App extends Component {
   state = {};
   render() {
     return (
-      <div className="App">
-        <Palette palette={generatePalette(seedColors[4])} />
-      </div>
+      <Switch>
+        <Route exact path="/" render={() => <h1>Palette List Goes here</h1>} />
+        <Route
+          exact
+          path="/palette/:id"
+          render={() => <h1>Individual Palette</h1>}
+        />
+      </Switch>
+
+      // <div className="App">
+      //   <Palette palette={generatePalette(seedColors[4])} />
+      // </div>
     );
   }
 }
