69990895bb7cb1aad41f111131c9d8f7d8fd4dc5
Nawanit <sharma.nawanit123@gmail.com>
2020-11-04 21:40:00 +0530

    create pallet component height fixed to 93%

--- src/DraggableColorBoxList.js
+++ src/DraggableColorBoxList.js
@@ -4,7 +4,7 @@ import DraggableColorBox from './DraggableColorBox';
 
 const DraggableColorList = SortableContainer(({ colors, removeColorBox }) => {
   return (
-    <div style={{ height: '96%' }}>
+    <div style={{ height: '93%' }}>
       {colors.map((color, i) => (
         <DraggableColorBox
           distance={1}
